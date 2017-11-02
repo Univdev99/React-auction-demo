5d99b33df0763e38b8ec6596de98874498ad5317
Univdev <univdev99@gmail.com>
2017-11-02 12:39:51 +0900

    [BE] Static root settings

--- backend/charibin/settings.py
+++ backend/charibin/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Django authentication done with email & password too
