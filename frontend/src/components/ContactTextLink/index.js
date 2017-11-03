c7c2f6f5802bc877d79c69642a32657fd561b0ed
Univdev <univdev99@gmail.com>
2017-11-03 18:15:53 -0200

    [FE] Hotfix contact text link url

--- frontend/src/components/ContactTextLink/index.js
+++ frontend/src/components/ContactTextLink/index.js
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default () => (
   <h4 className="mt-5 mb-5 text-center">
-    Do you have any questions? <Link to="/contact">Contact Us</Link>
+    Do you have any questions? <Link to="/support">Contact Us</Link>
   </h4>
 )
