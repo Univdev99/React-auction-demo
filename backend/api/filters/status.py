8a3ec5066db7aac7c954cccc53277e4179f08d24
Univdev <univdev99@gmail.com>
2017-11-06 07:39:10 -0200

    [BE] Update account-bid-auctions-list api endpoint to filter by status

--- backend/api/filters/status.py
+++ backend/api/filters/status.py
@@ -10,3 +10,14 @@ class StatusFilterBackend(filters.BaseFilterBackend):
         if status:
             queryset = queryset.filter(status=status)
         return queryset
+
+
+class AuctionBidStatusFilterBackend(filters.BaseFilterBackend):
+    """
+    Filters queryset with status parameter applied on its related bid model status field
+    """
+    def filter_queryset(self, request, queryset, view):
+        status = request.query_params.get('status')
+        if status:
+            queryset = queryset.filter(bid__status=status)
+        return queryset
