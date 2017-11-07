76f0ab585347eb14e3b1bd9078a7e45bb9c3b32a
Univdev <univdev99@gmail.com>
2017-11-07 19:26:58 -0200

    [FE] Change the order of success & fail callback

--- frontend/src/store/api/call.js
+++ frontend/src/store/api/call.js
@@ -49,22 +49,23 @@ export default ({
       onDownloadProgress,
     })
 
-    successCallback && successCallback(res)
-    success && success(res, action)
-
     yield put({
       type: requestSuccess(type),
       payload: payloadOnSuccess ? payloadOnSuccess(res.data, action) : res.data
     })
+
+    successCallback && successCallback(res)
+    success && success(res, action)
+
   } catch (err) {
     const errRes = get(err, 'response', err)
-
-    failCallback && failCallback(errRes)
-    fail && fail(errRes)
 
     yield put({
       type: requestFail(type),
       payload: payloadOnFail ? payloadOnFail(errRes, action) : errRes
     })
+
+    failCallback && failCallback(errRes)
+    fail && fail(errRes)
   }
 }
