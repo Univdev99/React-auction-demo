2bc1275d9ce16911126f3dd8963bc49448479073
Univdev <univdev99@gmail.com>
2017-11-29 12:28:09 -0200

    [FE] Implement Home banner styling

--- frontend/src/components/HomeBanner/index.js
+++ frontend/src/components/HomeBanner/index.js
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { Col, Container, Row } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import IconArrowRight from 'icons/IconArrowRight'
 
 
-const HomeBanner = () => (
-  <div className="bg-secondary text-light py-5">
-    <div className="container text-center">
-      <h2 className="mb-4">This is home banner!</h2>
+const COMPONENT_CLASS = 'home-banner'
+const HEADER_HEIGHT = 80
+const bem = (suffix) => `${COMPONENT_CLASS}__${suffix}`
 
-      <button className="btn btn-primary">Learn more</button>
-    </div>
-  </div>
-)
+class HomeBanner extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: window.innerHeight - HEADER_HEIGHT
+    }
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  handleResize = () => {
+    this.setState({ height: window.innerHeight - HEADER_HEIGHT })
+  }
+
+  render () {
+    const { height } = this.state
+
+    return (
+      <div className={COMPONENT_CLASS} style={{ height }}>
+        <video className={bem('video')} loop autoPlay poster="/videos/yuma-video-bg-poster.jpg">
+          <source src="/videos/yuma-video-bg.mp4" type="video/mp4" />
+          <source src="/videos/yuma-video-bg.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+        <div className={bem('overlay')} />
+        <Container className={bem('content')}>
+          <Row>
+            <Col xs={12} lg={8} xl={6}>
+              <h2 className={bem('title')}>Welcome to Yuma!</h2>
+              <p className={bem('description')}>
+                A do-gooder is what you can be with Yuma. We turn your things into lifesaving medicine,
+                emergency services, and support for any charitable organization you choose.
+              </p>
+              <Link to="/" className={bem('link')}>Learn more <IconArrowRight /></Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
 
 export default HomeBanner
---
+++ frontend/src/icons/IconArrowRight.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React, { Component } from 'react'
+import IconBase from 'react-icon-base'
+
+export default class IconArrowRight extends Component {
+  render() {
+    return (
+      <IconBase viewBox="0 0 22 8" {...this.props}>
+        <g stroke="none" strokeWidth="1" fillRule="evenodd">
+          <g>
+            <path d="M18.4631119,3 L0,3 L0,5 L18.4631119,5 L16.7700195,6.69309241 L18.0769271,8 L21.9976499,4.07927724 L21.9976499,3.92072276 L18.0769271,0 L16.7700195,1.30690759 L18.4631119,3 Z"></path>
+          </g>
+        </g>
+      </IconBase>
+    )
+  }
+}
