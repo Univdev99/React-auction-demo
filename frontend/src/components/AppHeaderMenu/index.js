be8662509909a4599737eb567f22a5ced09dbba4
Univdev <univdev99@gmail.com>
2017-11-29 12:54:56 -0200

    [FE] Update styling for menu dropdown

--- frontend/src/components/AppHeaderMenu/index.js
+++ frontend/src/components/AppHeaderMenu/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import cx from 'classnames'
 import { NavLink as Link } from 'react-router-dom'
-import { Nav, NavItem as BsNavItem, NavLink } from 'reactstrap'
-
-const COMPONENT_CLASS = 'app-header-menu'
-const bem = (suffix) => `${COMPONENT_CLASS}__${suffix}`
-
-const NavItem = ({ children }) => (
-  <BsNavItem className={bem('item')}>
-    {children}
-  </BsNavItem>
-)
+import { Nav, NavItem, NavLink } from 'reactstrap'
 
 const AppHeaderMenu = () => (
-  <Nav className={cx(COMPONENT_CLASS, 'ml-auto mr-3')} navbar>
+  <Nav className="ml-auto mr-3" navbar>
     <NavItem>
       <NavLink tag={Link} to="/auctions">Auctions</NavLink>
     </NavItem>
