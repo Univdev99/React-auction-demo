870cfbf1ce795bbf80a0cfee674041c5f6237c73
Univdev <univdev99@gmail.com>
2017-12-11 11:07:57 +0900

    [BE][FE] Admin products pagination

--- frontend/src/pages/AdminProductList/index.js
+++ frontend/src/pages/AdminProductList/index.js
@@ -5,7 +5,9 @@ import { createStructuredSelector } from 'reselect'
 import PropTypes from 'prop-types'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import { Link } from 'react-router-dom'
+import Immutable from 'immutable'
 
+import Pagination from 'components/Pagination'
 import SectionTitle from 'components/SectionTitle'
 import Spinner from 'components/Spinner'
 import {
@@ -13,6 +15,7 @@ import {
   deleteProduct,
 } from 'store/modules/admin/products'
 import { adminProductsSelector } from 'store/selectors'
+import { ADMIN_TABLE_PAGE_SIZE } from 'config'
 
 
 class AdminProductList extends PureComponent {
@@ -24,7 +27,8 @@ class AdminProductList extends PureComponent {
   }
 
   state = {
-    loadingStatus: 1
+    loadingStatus: 1,
+    page: 1,
   }
 
   handleDelete = (id, event) => {
@@ -45,6 +49,26 @@ class AdminProductList extends PureComponent {
     })
   }
 
+  changePage = (page) => {
+    const { loadingStatus } = this.state
+    if (loadingStatus !== 10) {
+      return
+    }
+    page = page < 1 ? 1 : page
+    this.setState({ page })
+  }
+
+  currentPageProductList = () => {
+    const { loadingStatus, page } = this.state
+    if (loadingStatus !== 10) {
+      return Immutable.List()
+    }
+
+    const { adminProducts } = this.props
+    const productList = adminProducts.get('productList')
+    return productList.slice((page - 1) * ADMIN_TABLE_PAGE_SIZE, page * ADMIN_TABLE_PAGE_SIZE)
+  }
+
   componentWillMount() {
     this.setState({
       loadingStatus: 1
@@ -63,7 +87,7 @@ class AdminProductList extends PureComponent {
   render() {
     const { adminProducts } = this.props
     const productList = adminProducts.get('productList')
-    const { loadingStatus } = this.state
+    const { loadingStatus, page } = this.state
 
     return (
       <div>
@@ -78,27 +102,38 @@ class AdminProductList extends PureComponent {
           Failed to load data.
         </div>}
 
-        {loadingStatus === 10 && <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productList.map(product => (
-              <tr key={product.get('pk')}>
-                <th scope="row">{product.get('pk')}</th>
-                <td>{product.get('title')}</td>
-                <td>
-                  <Link className="text-secondary pr-3" to={`/admin/products/${product.get('pk')}`}>Edit</Link>
-                  <a className="text-danger" href="/" onClick={this.handleDelete.bind(this, product.get('pk'))}>Delete</a>
-                </td>
+        {loadingStatus === 10 && <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>}
+            </thead>
+            <tbody>
+              {this.currentPageProductList().map(product => (
+                <tr key={product.get('pk')}>
+                  <th scope="row">{product.get('pk')}</th>
+                  <td>{product.get('title')}</td>
+                  <td>
+                    <Link className="text-secondary pr-3" to={`/admin/products/${product.get('pk')}`}>Edit</Link>
+                    <a className="text-danger" href="/" onClick={this.handleDelete.bind(this, product.get('pk'))}>Delete</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="mt-5 text-center">
+            <Pagination
+              currentPage={page}
+              totalCount={productList.size}
+              pageSize={ADMIN_TABLE_PAGE_SIZE}
+              onPage={this.changePage}
+            />
+          </div>
+        </div>}
       </div>
     )
   }
