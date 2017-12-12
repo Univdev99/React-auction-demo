42d4228344d67f7d0777b1999051efcb32ea6283
Univdev <univdev99@gmail.com>
2017-12-12 16:17:22 -0200

    [FE] Update content & styling of Terms&conditions page

--- frontend/src/pages/TermsConditions/index.js
+++ frontend/src/pages/TermsConditions/index.js
@@ -15,34 +15,44 @@ const TermsConditions = () => (
     subscribe
   >
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut nibh dictum, auctor libero ac,
-      varius sem. Aenean in augue sed enim pulvinar ultricies eget at nibh. Sed ac iaculis lorem. Donec
-      faucibus sodales risus, ac scelerisque urna tristique at. Etiam non nulla molestie mi pellentesque
-      rutrum. Fusce sodales tellus sit amet facilisis dictum. Sed sagittis vel dui condimentum dictum.
-      Cras ut purus in ligula fermentum convallis. Praesent non dolor imperdiet, rutrum mi in, rhoncus
-      neque. Maecenas sed gravida turpis. Proin commodo sem in arcu viverra lobortis. Morbi pulvinar at
-      ante sed vestibulum. Sed molestie mi nec odio pharetra finibus non non est. Vivamus cursus velit
-      leo, vel malesuada quam aliquam et.
+      This website (the "Site") is brought to you by Yuma, LLC. d/b/a Yuma (“Yuma”). Your use of the
+      Site is governed by these terms of use. Please read these terms carefully and do not use this
+      Site if you do not agree with these terms of use. The services provided on the Site include
+      auctions and the Custom Experiences and Do Good Programs (collectively, the "Custom Experiences"
+        Program) for a variety of property and experiences. By using this Site and participating in
+      these auctions and the Custom Experiences Program, YOU AGREE TO BE BOUND TO ANY PURCHASES YOU
+      WIN AND YOU FURTHER AGREE TO BE BOUND BY THESE TERMS OF USE.
     </p>
 
-    <h5 className="mb-3">Section Title</h5>
+    <h4>Operation of Site</h4>
     <p>
-      Pellentesque faucibus urna id nulla varius convallis. Interdum et malesuada fames ac ante ipsum
-      primis in faucibus. In congue hendrerit ex vel tincidunt. Suspendisse potenti. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus. Maecenas vestibulum tempor lacinia. Mauris orci libero,
-      fermentum eu varius eget, commodo eu ipsum. Integer ornare vitae odio egestas varius. Cras egestas
-      iaculis ligula, sit amet dignissim massa elementum et. Proin ut suscipit tellus, vel viverra orci.
-      Donec orci neque, laoreet nec risus non, imperdiet efficitur mauris. Duis a enim non neque sagittis
-      tempus quis vel diam. Cras hendrerit velit eget odio suscipit, suscipit tincidunt nibh tincidunt.
+      Once you register as a User on the Site, the Site offers several ways for a Yuma User to acquire
+      properties or experiences. The first way is by bidding through an online auction on the Site for
+      Property and/or an Experience (the "Auction") that is offered by a charitable organization or
+      third party donor (collectively, the "Seller") through Yuma. The second way is by submitting a
+      request to Yuma under the Yuma Custom Experiences Program for a specific Property and/or an
+      Experience. An "Experience" may include, but is not limited to, a celebrity meet and greet,
+      dinner with a celebrity or other such experience. "Property" is tangible property, including
+      without limitation artwork, apparel, food, wine, sports and music memorabilia. The term "Lot"
+      refers to either an Experience or a Property.
+    </p>
+    <h4>One More Section</h4>
+    <p>
+      It may be necessary − by law, legal process, litigation, and/or requests from public and
+      governmental authorities within or outside your country of residence − for Yuma to disclose
+      your personal information. We may also disclose information about you if we determine that for
+      purposes of national security, law enforcement, or other issues of public importance, disclosure
+      is necessary or appropriate.
     </p>
     <p>
-      Donec ut elit in enim imperdiet vehicula ut vel ex. Pellentesque enim purus, malesuada sed tempor in,
-      aliquet ut dolor. Aliquam gravida, leo eget bibendum gravida, nisl dui viverra lectus, quis pharetra
-      nulla sem et eros. Sed in purus sed eros euismod pellentesque ac eu metus. In viverra laoreet metus a
-      sodales. Quisque pulvinar sem sit amet enim molestie, in congue erat ultricies. Donec nec dolor nisl.
-      Nunc sed aliquet diam. Cras aliquam orci in placerat feugiat. Maecenas id justo nibh.
+      We may also disclose information about you if we determine that disclosure is reasonably necessary
+      to enforce our user agreement or protect our operations or users. Additionally, in the event of a
+      reorganization, merger, or sale we may transfer any and all personal information we collect to the
+      relevant third party. You will be notified via email and/or a prominent notice on our website, of
+      any change in ownership, uses of your personal information, and choices you may have regarding your
+      personal information.
     </p>
-
+  
     <ContactTextLink />
   </FrontContainerLayout>
 )
