6708f162985de45ccc7f8f1cb74c933ccab207b0
Univdev <univdev99@gmail.com>
2017-12-12 14:08:28 -0200

    [FE] Update styling & contents of Mission page

--- frontend/src/components/IconListItem/index.js
+++ frontend/src/components/IconListItem/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import cx from 'classnames'
 
-const IconListItem = ({ icon, children }) => (
-  <div className="d-flex mb-3">
-  	<div className="align-self-center mr-3 text-secondary">
-  		<i className={cx('fa', icon)} style={{ fontSize: '3rem' }} />
+const COMPONENT_CLASS = 'icon-list-item'
+const bem = (suffix) => `${COMPONENT_CLASS}__${suffix}`
+
+const IconListItem = ({ image, children }) => (
+  <div className={COMPONENT_CLASS}>
+  	<div className={bem('icon')}>
+      {image && <img src={image} alt="" />}
   	</div>
     <div className="align-self-center">{children}</div>
   </div>
