32a29af0de1e50d29a45481139a767c56de370cd
Univdev <univdev99@gmail.com>
2017-12-12 10:54:44 -0200

    [FE] Update styling of Job details page

--- frontend/src/pages/JobDetail/index.js
+++ frontend/src/pages/JobDetail/index.js
@@ -1,42 +1,24 @@
 import React, { PureComponent } from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import PropTypes from 'prop-types'
-import { Alert, Button, Col, Row } from 'reactstrap'
-import { compose } from 'redux'
+import { Alert } from 'reactstrap'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { reduxForm } from 'redux-form'
 
-import FormField from 'components/FormField'
 import FrontContainerLayout from 'layouts/FrontContainerLayout'
-import InputField from 'components/InputField'
-import SectionTitle from 'components/SectionTitle'
+import JobApplicationForm from 'components/JobApplicationForm'
+import Section from 'components/Section'
 import Spinner from 'components/Spinner'
-import TextareaField from 'components/TextareaField'
+import { API_PENDING, API_SUCCESS, API_FAIL } from 'store/api/request'
 import { getJobDetail } from 'store/modules/jobs'
 import { jobsSelector } from 'store/selectors'
-
-
-const isRequired = value => (value ? undefined : 'This Field is Required')
-const isValidEmail = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email address'
-    : undefined
 
 
 class JobDetail extends PureComponent {
 
   static propTypes = {
-    handleSubmit: PropTypes.func.isRequired,
     jobs: ImmutablePropTypes.map.isRequired,
     getJobDetail: PropTypes.func.isRequired,
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      status: 0, // 0: loading, 1: loaded, -1: error
-    }
   }
 
   breadcrumbPath(title) {
@@ -50,19 +32,7 @@ class JobDetail extends PureComponent {
   getJobDetail = (id) => {
     const { getJobDetail } = this.props
 
-    this.setState({
-      status: 0
-    })
-
-    getJobDetail({
-      id,
-      success: () => this.setState({
-        status: 1
-      }),
-      fail: () => this.setState({
-        status: -1
-      }),
-    })
+    getJobDetail({ id })
   }
 
   submitForm = (data) => {
@@ -74,9 +44,9 @@ class JobDetail extends PureComponent {
   }
 
   render() {
-    const { handleSubmit, jobs } = this.props
+    const { jobs } = this.props
     const jobDetail = jobs.get('jobDetail')
-    const { status } = this.state
+    const jobDetailStatus = jobs.get('jobDetailStatus')
     const title = jobDetail ? jobDetail.get('title') : ''
 
     return (
@@ -84,59 +54,21 @@ class JobDetail extends PureComponent {
         breadcrumbPath={this.breadcrumbPath(title)}
         subscribe
       >
-        {status === 0 && <Spinner />}
+        {jobDetailStatus === API_PENDING && <Spinner />}
 
-        {status === -1 && <Alert color="danger">Job not found</Alert>}
+        {jobDetailStatus === API_FAIL && <Alert color="danger">Job not found</Alert>}
 
-        {status === 1 && jobDetail && <div>
-          <SectionTitle className="mb-5">{title}</SectionTitle>
-
-          <div className="mb-5">
-            {jobDetail.get('description')}
-          </div>
-
-          <SectionTitle className="mt-5 mb-3">Apply For This Job</SectionTitle>
-          <form onSubmit={handleSubmit(this.submitForm)}>
-            <Row>
-              <Col xs={12} md={6}>
-                <FormField
-                  name="name"
-                  type="text"
-                  label="Name"
-                  validate={[isRequired]}
-                  component={InputField}
-                />
-              </Col>
-              <Col xs={12} md={6}>
-                <FormField
-                  name="email"
-                  type="email"
-                  label="Email"
-                  validate={[isRequired, isValidEmail]}
-                  component={InputField}
-                />
-              </Col>
-            </Row>
-            <FormField
-              name="message"
-              label="Message"
-              validate={[isRequired]}
-              component={TextareaField}
+        {jobDetailStatus === API_SUCCESS && jobDetail && <div>
+          <Section title={title}>
+            <div
+              className="page-content"
+              dangerouslySetInnerHTML={{ __html: jobDetail.get('description') }}
             />
-            <Row>
-              <Col xs={12} md={6}>
-                <FormField
-                  name="resume"
-                  label="Attach your resume"
-                  type="file"
-                  component={InputField}
-                />
-              </Col>
-              <Col xs={12} md={6} className="text-right">
-                <Button type="submit" color="primary">Submit</Button>
-              </Col>
-            </Row>
-          </form>
+          </Section>
+
+          <Section title="Apply For This Job">
+            <JobApplicationForm onSubmit={this.submitForm} />
+          </Section>
         </div>}
       </FrontContainerLayout>
     )
@@ -151,9 +83,4 @@ const actions = {
   getJobDetail
 }
 
-export default compose(
-  connect(selector, actions),
-  reduxForm({
-    form: 'jobApplicationForm'
-  })
-)(JobDetail)
+export default connect(selector, actions)(JobDetail)
