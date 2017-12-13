7f6637705706817dd67a447c44fc97a5e1e5489d
Univdev <univdev99@gmail.com>
2017-12-13 14:38:21 -0200

    [FE] Update styling of signup verification page

--- frontend/src/pages/SignUpVerification/index.js
+++ frontend/src/pages/SignUpVerification/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Col, Row } from 'reactstrap'
+import { Alert, Col, Row } from 'reactstrap'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -27,31 +27,30 @@ class SignUpVerification extends PureComponent {
       success: () => this.setState({
         status: 10
       }),
-      fail: () => this.setState({
-        status: -1
+      fail: ({ data }) => this.setState({
+        status: -1,
+        error: data.detail
       }),
     })
   }
 
   render() {
-    const { status } = this.state
+    const { status, error } = this.state
 
     return (
       <FrontContainerLayout subscribe>
         <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6}>
-            <center>
-              {status === 1 && <div>
-                  Verifying account...
-              </div>}
-              {status === 10 && <div>
-                  Your account is now verified, please sign in with the new account.<br/>
-                  <Link to="/signin">Please click here to Sign in.</Link>
-              </div>}
-              {status === -1 && <div>
-                  Account verification failed.
-              </div>}
-            </center>
+          <Col xs={12} md={8} lg={6} className="text-center">
+            {status === 1 && <h4>
+              Verifying account...
+            </h4>}
+            {status === 10 && <Alert color="success">
+              Your account is now verified, please sign in with the new account.<br/>
+              <Link to="/signin" className="alert-link">Please click here to Sign in.</Link>
+            </Alert>}
+            {status === -1 && <Alert color="danger">
+              {error || 'Account verification failed.'}
+            </Alert>}
           </Col>
         </Row>
       </FrontContainerLayout>
