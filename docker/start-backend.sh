00e12c36555189ba0cfae390f6d8832987738b58
Univdev <univdev99@gmail.com>
2017-12-23 18:13:39 +0900

    [Deploy] websocket config added

--- docker/start-backend.sh
+++ docker/start-backend.sh
@@ -10,5 +10,11 @@ python manage.py migrate
 # static assets
 python manage.py collectstatic --noinput
 
+# boot daphne for django-channels
+daphne -b 0.0.0.0 -p 8001 charibin.asgi:channel_layer &
+
+# django-channels runworker
+python manage.py runworker &
+
 # now boot
 gunicorn charibin.wsgi:application -b 0.0.0.0:8000 -t 300 --access-logfile /app/log/access.log --error-logfile /app/log/error.log
